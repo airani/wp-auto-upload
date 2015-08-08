a0b35802f40dc98a98edca700302bbae162018ee
Sergey Funn <racypepper@gmail.com>
2015-08-08 23:45:38 +0300

    Add user agent

--- index.php
+++ index.php
@@ -93,7 +93,9 @@ class WP_Auto_Upload {
         }
 
         setlocale(LC_ALL, "en_US.UTF8");
+        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
         $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
